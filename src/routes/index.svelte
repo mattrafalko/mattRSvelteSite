901bdf906c42978779dd7e1eb4a4ae93097356b6
Matt Rafalko <mattrafalko@gmail.com>
2021-10-23 09:50:16 -0500

    Added animation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import {slide } from 'svelte/transition';
 	const links = [
 		{
 			name: 'email',
@@ -13,20 +14,26 @@
 			url: 'https://www.linkedin.com/in/mrafalko'
 		}
 	];
+
+	import { onMount } from 'svelte';
+
+	let ready = false;
+	onMount(() => (ready = true));
 </script>
 
 <div class="mx-auto max-w-5xl font-lato text-red-300 px-10 md:px-0 ">
 	<div class="flex flex-col place-content-center h-screen">
 		<div>
 			<h1 class="text-4xl font-black italic mb-16">matt rafalko</h1>
-
-			<ul class="space-y-4 w-24">
-				{#each links as link}
-					<li class="font-semibold text-xl hover:text-red-200">
-						<a href={link.url} target="_blank">{link.name}</a>
-					</li>
-				{/each}
-			</ul>
+			{#if ready}
+				<ul class="space-y-4 w-24">
+					{#each links as link}
+						<li transition:slide="{{duration: 1250}}" class="font-semibold text-xl hover:text-red-200">
+							<a href={link.url} target="_blank">{link.name}</a>
+						</li>
+					{/each}
+				</ul>
+			{/if}
 		</div>
 	</div>
 </div>
