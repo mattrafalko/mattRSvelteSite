6208ac8c53183ff1760aa1a8f65d00ba04ebd0fe
Matt Rafalko <37773503+mattrafalko@users.noreply.github.com>
2022-07-03 10:04:53 -0500

    styling changes 7/3/22 (#3)
    * styling changes to get rid of page scrolling; using fly instead of slide animation
    
    * added viewport-fit cover to account for device notches
    
    * forgot to add padding for notch
    
    * ...and added viewport-fit: cover css property
    
    * correction to meta tag
    
    * top notch fix.
    
    * added .env to git ignore
    
    * remove safe-area-inset top and bottom
    
    * added apple mea tags
    
    * update initial scale
    
    * revert initial scale, targeting html styling
    
    * add theme color
    
    * update theme color
    
    * update theme color
    
    Co-authored-by: Matt Rafalko <mattr@MATT-R-PC-FAST.localdomain>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,36 +1,37 @@
 <script>
-import '../styles/tailwind.css'
-import SvelteSeo from 'svelte-seo'
+	import '../styles/tailwind.css';
+	import SvelteSeo from 'svelte-seo';
 </script>
 
 <svelte:head>
-    <title>matt r</title>
+	<title>matt r</title>
 </svelte:head>
 
-<SvelteSeo
-    title="matt r"
-    description="matt rafalko - software engineer"
-/>
+<SvelteSeo title="matt r" description="matt rafalko &#9679; software engineer" />
 
-<slot></slot>
+<slot />
 
 <style global lang="postcss">
-    
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 
-    body {
-        /* @apply bg-black; */
-        @apply bg-green-100 dark:bg-black font-bree text-green-700 dark:text-green-50 antialiased mx-auto max-w-5xl p-10;
-    }
+	html {
+		min-height: calc(100% + env(safe-area-inset-top));
+		padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom)
+			env(safe-area-inset-left);
+	}
 
-    h1 {
+	body {
+		/* @apply bg-black; */
+		@apply bg-green-100 dark:bg-black font-bree text-green-700 dark:text-green-50 antialiased mx-auto max-w-5xl;
+	}
+
+	h1 {
 		@apply text-6xl tracking-widest;
 	}
 
-
-
-
+	#svelte {
+		@apply h-screen flex;
+	}
 </style>
-
