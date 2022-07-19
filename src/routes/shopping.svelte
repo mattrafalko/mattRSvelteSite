f6f6631ff659ba4596deaa19f7372a61043af599
Matt Rafalko <37773503+mattrafalko@users.noreply.github.com>
2022-07-19 14:34:15 -0500

    used SSR on shopping page (#6)
    * used SSR on shopping page
    
    * styling updates to cards

--- src/routes/shopping.svelte
+++ src/routes/shopping.svelte
@@ -1,26 +1,30 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		let response = await fetch('https://fakestoreapi.com/products');
+		let data = await response.json();
+		if (response.ok) {
+			return {
+				props: {
+					items: data
+				}
+			};
+		}
+	};
+</script>
+
 <script>
 	import ItemCard from '../components/ItemCard.svelte';
-	import { onMount } from 'svelte';
 	import { cart } from '../stores/cart';
 	import { fade } from 'svelte/transition';
 
-	let data = [];
-
-	const getData = async () => {
-		let response = await fetch('https://fakestoreapi.com/products');
-		return await response.json();
-	};
-
-	onMount(async () => {
-		data = await getData();
-	});
+	export let items;
 
 	const removeItem = (item) => {
 		$cart = $cart.filter((x) => x.id !== item.id);
 	};
 </script>
 
-<div class="">
+<div class="mx-auto">
 	<div class="">
 		<details class="transtion-all mb-8">
 			<summary class="hover:cursor-pointer">cart: {$cart.length}</summary>
@@ -48,9 +52,9 @@
 			</p>
 		</details>
 	</div>
-	<div class="flex flex-col mx-auto max-w-3xl h-screen">
-		{#each data as item, i}
-			<ItemCard {item} />
+	<div class="flex flex-col mx-auto max-w-3xl">
+		{#each items as item}
+			<ItemCard {item} class="last:pb-16" />
 		{:else}
 			<p class="mx-auto" transition:fade>loading items...</p>
 		{/each}
